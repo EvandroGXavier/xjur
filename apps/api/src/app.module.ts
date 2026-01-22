fc24e7c085802dc650da7a2d61f7b78edea362c6
EvandroGXavier <evandro@conectionmg.com.br>
2026-01-22 20:10:13 -0300

    feat: implementação global do PrismaModule para estabilidade da API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '@dr-x/database';
 import { TemplatesModule } from './templates/templates.module';
 import { DocumentsModule } from './documents/documents.module';
+import { WhatsAppModule } from './whatsapp/whatsapp.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
   imports: [
     PrismaModule, 
     TemplatesModule,
     DocumentsModule,
+    WhatsAppModule,
+    ContactsModule,
   ],
 })
 export class AppModule {}
