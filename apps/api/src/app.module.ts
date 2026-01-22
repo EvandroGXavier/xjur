66c1b1491b4a5f3cb74e61ea2105ab51b9952f43
EvandroGXavier <evandro@conectionmg.com.br>
2026-01-22 19:54:31 -0300

    fix: ajuste de caminhos relativos no app.module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@dr-x/database';
+// Ajustando os caminhos para serem relativos à pasta onde este arquivo está
 import { TemplatesModule } from './templates/templates.module';
 import { DocumentsModule } from './documents/documents.module';
 
 @Module({
   imports: [
-    PrismaModule, // O banco Global que você sugeriu
+    PrismaModule, 
     TemplatesModule,
     DocumentsModule,
   ],
