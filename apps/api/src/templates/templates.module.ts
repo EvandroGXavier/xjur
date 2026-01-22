34991fa5f6b1a70d192b8ac5e18965968af7c5fd
EvandroGXavier <evandro@conectionmg.com.br>
2026-01-22 18:48:19 -0300

    fix: resolve dependência do PrismaService no TemplatesModule

--- apps/api/src/templates/templates.module.ts
+++ apps/api/src/templates/templates.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TemplatesService } from './templates.service';
 import { TemplatesController } from './templates.controller';
+import { PrismaModule } from '@dr-x/database'; // Importando a conexão com o banco
 
 @Module({
+  imports: [PrismaModule], // Adicionando aqui para resolver o erro do log
   controllers: [TemplatesController],
   providers: [TemplatesService],
+  exports: [TemplatesService],
 })
 export class TemplatesModule {}
