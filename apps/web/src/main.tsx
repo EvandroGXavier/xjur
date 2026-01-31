7af79e556bbbf8f8ec219792496f05b08f9f63b6
EvandroGXavier <evandro@conectionmg.com.br>
2026-01-31 00:13:04 -0300

    feat: Adiciona o ponto de entrada principal da aplicação NestJS, configurando CORS e validação global com `ValidationPipe`.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  
+  // Configurar validação global
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,           // Remove propriedades não definidas no DTO
+    forbidNonWhitelisted: true, // Retorna erro se propriedades extras forem enviadas
+    transform: true,            // Transforma payloads em instâncias de DTO
+    transformOptions: {
+      enableImplicitConversion: true, // Converte tipos automaticamente
+    },
+  }));
+  
+  await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000');
+}
+bootstrap();
