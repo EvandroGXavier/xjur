a48e94658a77c9a6a6df027f8fede4c53b0704b4
EvandroGXavier <evandro@conectionmg.com.br>
2026-01-31 12:54:52 -0300

    feat: Cria o ponto de entrada principal `main.tsx` para a aplicação web.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,22 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  
-  // Configurar validação global
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,           // Remove propriedades não definidas no DTO
-    forbidNonWhitelisted: true, // Retorna erro se propriedades extras forem enviadas
-    transform: true,            // Transforma payloads em instâncias de DTO
-    transformOptions: {
-      enableImplicitConversion: true, // Converte tipos automaticamente
-    },
-  }));
-  
-  await app.listen(3000);
-  console.log('Application is running on: http://localhost:3000');
-}
-bootstrap();
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
